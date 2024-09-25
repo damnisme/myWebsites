3eedafe59b3d3d71b74580e496fc2ea8959f30c2
hadhami_7075 <113293224+damnisme@users.noreply.github.com>
2024-09-25 12:54:45 +0200

    b

---
+++ DIFF.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+<body>
+    
+    <?php
+    function sayHello($name)
+    {
+        return "Hello world!";
+    }
+
+    $test = sayHello("Daniel");
+    echo $test;
+    
+    ?>
+
+</body>
+</html>
