b7039e13a600a8341189a29f7a54ca981a5063a0
hadhami_7075 <113293224+damnisme@users.noreply.github.com>
2024-09-25 22:25:34 +0200

    main

--- DIFF.php
+++ DIFF.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,17 +9,35 @@
 
 
 <body>
-    
-    <?php
+
+    <h3>Signup</h3>
+    <form action="./dbh.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <br>
+        <input type="password" name="pwd" placeholder="Password">
+        <br>
+        <input type="text" name="email" placeholder="E-Mail">
+        <br>
+        <button>Signup</button>
+    </form>
+
+
+
+
+
+
+
+
+
+    <!-- <?php
     function sayHello($name)
     {
         return "Hello world!";
     }
 
     $test = sayHello("Daniel");
-    echo $test;
-    
-    ?>
+    echo $test; ?> -->
 
 </body>
+
 </html>
